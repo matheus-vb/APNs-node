b067368cc0df875c993f15111e5b92c1b7e17185
Matheus Batista <matheus.vbatista04@gmail.com>
2023-07-28 18:43:38 -0300

    chore: create env handler

---
+++ source/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3001),
+    KEY_ID: z.string(),
+    TEAM_ID: z.string(),
+    BUNDLE_ID: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+    console.error("Invalid environment variables", _env.error.format());
+
+    throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data
